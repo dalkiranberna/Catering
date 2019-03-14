c64f3b3008478bc6898036de8ebeade83286989d
Berna <Wissen302>
2019-03-14 14:55:38 +0300

    Profile-2

--- Catering/Catering/Controllers/MyAccountController.cs
+++ Catering/Catering/Controllers/MyAccountController.cs
@@ -1,4 +1,6 @@
 ﻿using BusinessLogicLayer;
+using Entity;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,7 @@ namespace Catering.Controllers
 {
     public class MyAccountController : Controller
     {
+
 		UnitOfWork _uw = new UnitOfWork();
         public ActionResult Index()
         {
@@ -16,9 +19,16 @@ namespace Catering.Controllers
             return View();
         }
 
-        public ActionResult Proile()
+        public ActionResult Profile()
         {
-            return View();
-        }
+			string uId = User.Identity.GetUserId();
+			Member member = _uw.db.Users.Find(uId);
+			MyAccountViewModel vm = new MyAccountViewModel();
+			vm.Email = member.Email;
+			vm.PhoneNumber = member.PhoneNumber;
+			if (member.HasPhoto)
+				ViewBag.Photo = "/Uploads/Members/" + uId + ".jpg";
+			return View(vm);
+		}
     }
 }
