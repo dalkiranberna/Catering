d5147432375cd9bf4bc2888e7c2abdfc58bf22dd
Berna <Wissen302>
2019-03-25 14:54:29 +0300

    shopping cart

--- Catering/Catering/Controllers/CartController.cs
+++ Catering/Catering/Controllers/CartController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLogicLayer;
 using DataAccessLayer;
+using Entity;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +10,30 @@ using System.Web.Mvc;
 
 namespace Catering.Controllers
 {
-    public class CartController : Controller
-    {
-        UnitOfWork _uw = new UnitOfWork();
+	public class CartController : Controller
+	{
+		UnitOfWork _uw = new UnitOfWork();
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+		public ActionResult Index()
+		{
+			if (User.Identity.IsAuthenticated)
+			{
+				string uId = User.Identity.GetUserId();
+				Member m = _uw.db.Users.Find(uId);
+
+				if (m.ShoppingCart == null)
+				{
+					m.ShoppingCart = new ShoppingCart();
+					m.ShoppingCart.ProductItems = new List<ProductItem>();
+				}
+				return View(m.ShoppingCart);
+			}
+			return RedirectToAction("Index", "Home");
+		}
+
+		public ActionResult Delete()
+		{
+			return View();
+		}
+	}
 }
