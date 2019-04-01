eda36eb845da0b5f9ee71b74ff6a5bf7a37ed17a
Berna <Asus@DESKTOP-U25LG74>
2019-04-01 06:39:26 +0300

    CreateProduct

--- Catering/Catering/Content/MyAccount/Store/MultiImgUpload/js/app.js
+++ Catering/Catering/Content/MyAccount/Store/MultiImgUpload/js/app.js
@@ -16,7 +16,7 @@
     'use strict';
 
     var isOnGitHub = window.location.hostname === 'blueimp.github.io',
-        url = isOnGitHub ? '//jquery-file-upload.appspot.com/' : 'server/php/';
+        url = isOnGitHub ? '//jquery-file-upload.appspot.com/' : 'Index/';
 
     angular.module('demo', [
         'blueimp.fileupload'
